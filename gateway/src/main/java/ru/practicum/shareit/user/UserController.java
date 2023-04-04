57b8b884043baea8b0d85ba043338529047f88b6
misandr <misandr@yandex.ru>
2023-04-04 20:07:51 +0300

    feat: спринт 16.

--- gateway/src/main/java/ru/practicum/shareit/user/UserController.java
+++ gateway/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,9 +1,7 @@
 package ru.practicum.shareit.user;
 
-import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
@@ -11,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.exceptions.*;
 
 import javax.validation.Valid;
-import java.nio.charset.StandardCharsets;
 
 @Controller
 @RequestMapping(path = "/users")
@@ -22,7 +19,7 @@ public class UserController {
     private final UserClient userClient;
 
     @PostMapping
-    public ResponseEntity<User> addUser(@RequestBody @Valid User user) {
+    public ResponseEntity<Object> addUser(@RequestBody @Valid User user) {
         if (user == null) {
             log.warn("Request for add user is empty!");
             throw new NullValidationException("User");
@@ -37,31 +34,8 @@ public class UserController {
             log.warn("Email is null!");
             throw new NullValidationException("Email");
         }
-        ResponseEntity<Object> r = userClient.addUser(user);
 
-        // Так?
-        Gson gson = new Gson();
-        if (r.getStatusCode() == HttpStatus.OK) {
-
-            User gotUser = gson.fromJson(r.getBody().toString(), User.class);
-            log.info("Add new user {}", user);
-            return new ResponseEntity<User>(gotUser, r.getHeaders(), r.getStatusCode());
-        } else {
-            String responseString = new String((byte[]) r.getBody(), StandardCharsets.UTF_8);
-            ErrorResponse errorResponse = gson.fromJson(responseString, ErrorResponse.class);
-            switch (r.getStatusCode().value()) {
-                case 400:
-                    throw new ValidationException(errorResponse.getError());
-                case 403:
-                    throw new ForbiddenException(errorResponse.getError());
-                case 404:
-                    throw new NotFoundException(errorResponse.getError());
-                case 409:
-                    throw new ConflictException(errorResponse.getError());
-                default:
-                    throw new RuntimeException(errorResponse.getError());
-            }
-        }
+        return userClient.addUser(user);
     }
 
     @PatchMapping("/{userId}")
